0f318ea986c0bbaa61d654f407d0eeb990e8d227
Wesolutki <terlikowski.grzegorz@gmail.com>
2012-04-09 22:42:25 +0200

    unit generalization for other unit systems

--- src/si.h
+++ src/si.h
@@ -10,50 +10,35 @@ struct Dimension
 	enum { n=N, l=L };
 };
 
-template<int M, int KG, int S>
-struct Unit { // a unit in the MKS system
-    //enum { m=M, kg=KG, s=S };
-	/*constexpr static string text()
-	{
-		string str("purek   ");
-		str.push_back(' ');
-		str.push_back(M+48);
-		str.push_back(KG+48);
-		str.push_back(S+48);
-		return str;
-	}*/
-	typedef Dimension<M, 1> dim_m;
-	typedef Dimension<KG, 2> dim_kg;
-	typedef Dimension<S, 1> dim_s;
-	constexpr static int dims[3] = {M, KG, S};
-	enum { m = dim_m::n };
-	enum { kg = dim_kg::n };
-	enum { s = dim_s::n };
-	typedef typename UnitTextImpl<0, true, StringBuilder<>, M, KG, S>::str str;
-
-	/*typedef StringBuilder<'u', 'n', 'i', 't', ' '> init_text;
-	typedef typename init_text::template cond_add_char<(m<0), '/'>::str text_m1;
-	typedef typename text_m1::template cond_add_char<(m>0), '*'>::str text_m2;
-	typedef typename text_m2::template cond_add_char<m!=0, 'm'>::str text_m3;
-	typedef typename text_m3::template cond_add_char<(kg<0), '/'>::str text_k1;
-	typedef typename text_k1::template cond_add_char<(kg>0), '*'>::str text_k2;
-	typedef typename text_k2::template cond_add_chars<kg!=0, 'k', 'g'>::str text_k3;
-	typedef typename text_k3::template cond_add_char<(s<0), '/'>::str text_s1;
-	typedef typename text_s1::template cond_add_char<(s>0), '*'>::str text_s2;
-	typedef typename text_s2::template cond_add_char<s!=0, 's'>::str unit;
-*/
+template<const char* Texts, const int* Lens, int... Dims>
+struct Unit
+{
+	typedef typename UnitTextImpl<Texts, Lens, 0, true, StringBuilder<>, Dims...>::str str;
 	constexpr static const char * text = str::value;
-	/*	typename StringBuilder<'u', 'n', 'i', 't', ' '>::template
-		typename cond_add_char<m!=0, 'm'>::template str::
-		cond_add_char<kg!=0, 'k'>::str::value;*/
-		//cond_add_char<s!=0, 's'>::str::value;
+};
+
+template<int M, int KG, int S>
+struct UnitSI 
+{
+	enum { m=M, kg=KG, s=S };
+	
+	// dimension texts constrains
+	constexpr static char SITexts[] = {	'm',
+										'k', 'g',
+										's'};
+
+	constexpr static int SITextLengths[] = {	1,
+												2,
+												1};
+	
+	constexpr static const char * text = Unit<SITexts, SITextLengths, M, KG, S>::text;
 };
 
 template<typename U1, typename U2>
-using UnitMul = Unit<U1::m + U2::m, U1::kg + U2::kg, U1::s + U2::s>;
+using UnitMul = UnitSI<U1::m + U2::m, U1::kg + U2::kg, U1::s + U2::s>;
 
 template<typename U1, typename U2>
-using UnitDiv = Unit<U1::m - U2::m, U1::kg + U2::kg, U1::s - U2::s>;
+using UnitDiv = UnitSI<U1::m - U2::m, U1::kg + U2::kg, U1::s - U2::s>;
 
 /*template<typename U1, typename U2>
 constexpr UnitDiv<U1, U2> operator/ (const U1 & u1, const U2 & u2)
@@ -63,48 +48,48 @@ constexpr UnitDiv<U1, U2> operator/ (const U1 & u1, const U2 & u2)
 
 //using SpeedUnit2 = Unit<1,0,0>() / Unit<0,0,1>();
 
-template<typename Unit> // magnitude with unit
+template<typename U> // magnitude with unit
 struct Value {
-	typedef Unit unit;
+	typedef U unit;
     double _val;    // the magnitude
     constexpr explicit Value(double d)
          : _val(d) {} // construct a Value from a double
-	constexpr Value<Unit> operator+ (const Value<Unit> & v)
+	constexpr Value<U> operator+ (const Value<U> & v)
 	{
-		return Value<Unit> (_val + v._val);
+		return Value<U> (_val + v._val);
 	}
 	
 	template <typename ValueIn>
-	constexpr Value<UnitDiv<Unit, typename ValueIn::unit>> operator/ (const ValueIn & v)
+	constexpr Value<UnitDiv<U, typename ValueIn::unit>> operator/ (const ValueIn & v)
 	{
-		return Value<UnitDiv<unit, typename ValueIn::unit>> (_val / v._val);
+		return Value<UnitDiv<U, typename ValueIn::unit>> (_val / v._val);
 	}
 	
 	template <typename ValueIn>
-	constexpr Value<UnitMul<Unit, typename ValueIn::unit>> operator* (const ValueIn & v)
+	constexpr Value<UnitMul<U, typename ValueIn::unit>> operator* (const ValueIn & v)
 	{
-		return Value<UnitMul<Unit, typename ValueIn::unit>> (_val * v._val);
+		return Value<UnitMul<U, typename ValueIn::unit>> (_val * v._val);
 	}
 	
-	template<typename U>
-	friend ostream & operator << (ostream & str, const Value<U> & p);
+	template<typename U2>
+	friend ostream & operator << (ostream & str, const Value<U2> & p);
 };
 
-template<typename U>
-ostream &  operator << (ostream & str, const Value<U> & p)
+template<typename U2>
+ostream &  operator << (ostream & str, const Value<U2> & p)
 {
-    str << p._val << " " << U::text;
+    str << p._val << " " << U2::text;
 	return str;
 }
 
-using Second = Unit<0,0,1>;  // s
-using Second2 = Unit<0,0,2>; // s*s
-using Meter = Unit<1,0,0>;
+using Second = UnitSI<0,0,1>;  // s
+using Second2 = UnitSI<0,0,2>; // s*s
+using Meter = UnitSI<1,0,0>;
 using Distance = Value<Meter>;
 using Time = Value<Second>;
 using SpeedUnit = UnitDiv<Meter, Second>;
 using Speed = Value<SpeedUnit>;        // m /s
-using Acceleration = Value<Unit<1,0,-2>>; // m /s/s
+using Acceleration = Value<UnitSI<1,0,-2>>; // m /s/s
 
 //using SpeedUnit2 = Meter / Second;
 
--- src/str.h
+++ src/str.h
@@ -49,68 +49,63 @@ const char StringBuilder<S...>::value[sizeof...(S)+1] = {
 	S...
 };
 
-// dimension texts constrains
-constexpr char dimensionTexts[] = {	'm',
-									'k', 'g',
-									's'};
-
-constexpr int dimensionTextLengths[] = {	1,
-											2,
-											1};
-
 // dimension text helper
 // recursion through text lengths
-template <int N, int Start, int Len, typename B>
+template <const char* Texts, const int* Lens, int N, int Start, int Len, typename B>
 struct DimensionTextHelper
 {
-	typedef typename DimensionTextHelper<N-1, Start+dimensionTextLengths[N-1], Len,B>::str str;
+	typedef typename DimensionTextHelper<Texts, Lens, N-1, Start+Lens[N-1], Len,B>::str str;
 };
 
 // and StringBuilder add_char concatenation
-template <int Start, int Len, typename B>
-struct DimensionTextHelper<0, Start, Len, B>
+template <const char* Texts, const int* Lens, int Start, int Len, typename B>
+struct DimensionTextHelper<Texts, Lens, 0, Start, Len, B>
 {
-	typedef typename DimensionTextHelper<0, Start+1, Len-1, typename B::template add_char<dimensionTexts[Start]>::str>::str str;
+	typedef typename DimensionTextHelper<Texts, Lens, 0, Start+1, Len-1, typename B::template add_char<Texts[Start]>::str>::str str;
 };
 
-template <int Start, typename B>
-struct DimensionTextHelper<0, Start, 0, B>
+template <const char* Texts, const int* Lens, int Start, typename B>
+struct DimensionTextHelper<Texts, Lens, 0, Start, 0, B>
 {
 	typedef B str;
 };
 
 // unit text helper
 // conditions for first and (-)1 dimension
-template<int N, typename B, int D, bool is_first>
+template<const char* Texts, const int* Lens, int N, typename B, int D, bool is_first>
 struct UnitTextHelper
 {
 	typedef typename B::template cond_add_char<(is_first==true & D<0), '/'>::str begin;
 	typedef typename begin::template cond_add_char<(is_first==false & D<0), '/'>::str div;
 	typedef typename div::template cond_add_char<(is_first==false & D>0), '*'>::str mul;
-	typedef typename DimensionTextHelper<N, 0, dimensionTextLengths[N], mul>::str dimension;
+	typedef typename DimensionTextHelper<Texts, Lens, N, 0, Lens[N], mul>::str dimension;
 	typedef typename dimension::template cond_add_char<(D != 1 & D != -1 & D<0), (char)-D+48>::str min_val;
 	typedef typename min_val::template cond_add_char<(D != 1 & D != -1 & D>0), (char)D+48>::str str;
 };
 
-template<int N, typename B, bool is_first>
-struct UnitTextHelper<N, B, 0, is_first>
+template<const char* Texts, const int* Lens, int N, typename B, bool is_first>
+struct UnitTextHelper<Texts, Lens, N, B, 0, is_first>
 {
 	typedef B str;
 };
 
 // unit text implementation
 // created using recursive dimension execution
-template<int N, bool is_first, typename B, int...>
+template<const char* Texts, const int* Lens, int N, bool is_first, typename B, int...>
 struct UnitTextImpl;
 
-template<int N, bool is_first, typename B, int Head, int... Tail>
-struct UnitTextImpl<N, is_first, B, Head, Tail...>
+template<const char* Texts, const int* Lens, int N, bool is_first, typename B, int Head, int... Tail>
+struct UnitTextImpl<Texts, Lens, N, is_first, B, Head, Tail...>
 {
-	typedef typename UnitTextImpl<N+1, (is_first==true & Head==0), typename UnitTextHelper<N, B, Head, is_first>::str, Tail...>::str str;
+	typedef typename UnitTextImpl<	Texts, 
+									Lens, 
+									N+1, 
+									(is_first==true & Head==0), 
+									typename UnitTextHelper<Texts, Lens, N, B, Head, is_first>::str, Tail...>::str str;
 };
 
-template<int N, bool is_first, typename B>
-struct UnitTextImpl<N, is_first, B>
+template<const char* Texts, const int* Lens, int N, bool is_first, typename B>
+struct UnitTextImpl<Texts, Lens, N, is_first, B>
 {
 	typedef B str;
 };
